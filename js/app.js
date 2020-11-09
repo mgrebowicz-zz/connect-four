93ca361c79b0a6858a6ecc256bbe72fed04db8df
Michael Grebowicz <maciejgrebowicz@gmail.com>
2020-11-08 21:41:10 -0800

    modify styles.css

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//*----- constants -----*/
+const COLORS = {
+    null: 'white', 
+    '1': 'red', 
+    '-1': 'yellow'
+}
+
+
+/*----- app's state (variables) -----*/ 
+let board; 
+let turn; 
+let winner;
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
